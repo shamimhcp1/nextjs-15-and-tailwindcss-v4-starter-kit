6c495772e47f8ecd19368ac42acd8782332a1296
shamimhcp1 <shamimhcp1@gmail.com>
2025-08-29 23:04:01 +0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Button</Button>
+      <ModeToggle />
+    </div>
+  );
+}
